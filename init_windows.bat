a319072006fe54215b85918f9703c0e9527abae8
VictorDalosto <victordalosto@gmail.com>
2023-01-08 19:40:10 -0300

    removing spaces

--- init_windows.bat
+++ init_windows.bat
@@ -1,9 +1,5 @@
-@echo   + ========================= +
-@echo       BOOSTRAP FOR WINDOWS
-@echo   + ========================= +
+@echo BOOSTRAP FOR WINDOWS
 
-
-@REM ###############################
 @net session >nul 2>&1 & :: Checks for admin permission
 @if NOT %errorLevel% == 0 (
     @echo ERROR: File should be executed with administrator right!
@@ -12,20 +8,13 @@
 )
 @cd %~dp0 & :: Changes current directory to original local of .bat
 
-
-@REM ###############################
 @echo  ## INSTALL CHOCOLATEY 
-@REM ###############################
 @cmd /c bin\choco_install
 @cmd /c bin\choco_install_package
 @cmd /c bin\choco_upgrade
 
-
-@REM ###############################
 @echo ## VSCODE EXTENSIONS & SETTINGS
-@REM ###############################
 @cmd /c bin\vscode_extension
 @cmd /c bin\vscode_settings
-
 
 @pause
