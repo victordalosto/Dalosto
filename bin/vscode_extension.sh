900739e0229ef8383e2d53d900ae5e52a957b54e
VictorDalosto <victordalosto@gmail.com>
2023-01-14 09:18:23 -0300

    added other packages

--- bin/vscode_extension.sh
+++ bin/vscode_extension.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 # Install Visual studio Code extensions.
 # OS: LINUX-UBUNTU
-cat ./vscode/extensions.txt | while read extension || [[ -n  $extension ]];
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DIR="$(dirname "$SCRIPT_DIR")"
+cat $DIR/vscode/extensions.txt | while read extension || [[ -n  $extension ]];
 do
 	code --install-extension $extension
 done 
